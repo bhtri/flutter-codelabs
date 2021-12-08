0dcc4114dfac3040024aeacc58a6ef758fe9ef4f
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-12-08 11:57:14 +0900

    ✅ part 02

--- lab01/README.md
+++ lab01/README.md
@@ -3,3 +3,4 @@
 A new Flutter project.
 
 https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1
+https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2
--- lab01/lib/random_words.dart
+++ lab01/lib/random_words.dart
@@ -10,6 +10,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = [];
+  final Set<WordPair> _saved = {};
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -17,6 +18,13 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.list),
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -36,10 +44,59 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        // Tip: In Flutter's reactive style framework,
+        // calling setState() triggers a call to the build() method for the State object,
+        // resulting in an update to the UI.
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map((pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          });
+          final List<Widget> divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  tiles: tiles,
+                  context: context,
+                ).toList()
+              : [];
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(
+              children: divided,
+            ),
+          );
+        },
       ),
     );
   }
