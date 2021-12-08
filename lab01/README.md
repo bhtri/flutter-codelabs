eaba70b4a63a4038bf100557dd7aa7cc82cd5424
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-12-08 10:23:36 +0900

    🎉 init lab01
    part 01

---
+++ lab01/README.md
@@ -0,0 +1,5 @@
+# lab01
+
+A new Flutter project.
+
+https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1
---
+++ lab01/android/app/src/main/kotlin/com/bhtri/lab01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bhtri.lab01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
